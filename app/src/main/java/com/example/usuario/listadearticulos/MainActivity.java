3e08b5874bc2534fc9e2ea00ab0ae2f660b31e2a
jorge moutinho <Kantz0rd93@gmail.com>
2018-11-04 21:25:58 -0400

    Documentacion

--- app/src/main/java/com/example/usuario/listadearticulos/MainActivity.java
+++ app/src/main/java/com/example/usuario/listadearticulos/MainActivity.java
@@ -4,14 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+/**
+
+ * Esta clase muestra y define objetos seleccionados por el usuario
+
+ */
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    /**
+     * Método que muestra la pantalla principal de la aplicacion
+     */
+        TextView product;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            product = (TextView) findViewById(R.id.replyprod);
+
+            if (savedInstanceState != null) {
+                boolean isVisible = savedInstanceState.getBoolean("list_visible");
+                if (isVisible) {
+
+                    product.setVisibility(View.VISIBLE);
+                    product.setText(savedInstanceState.getString("list_text"));
+                    product.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    //Cierre del método
+
+    /**
+     * Método que muestra los articulos seleccionados
+     */
 
     public void listView(View view){
         Intent intent = new Intent(this, listActivity.class);
@@ -19,5 +45,19 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-}
+    //Cierre del método
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Si el encabezado es visible, tenemos un mensaje que necesita ser
+        // guardado.
+        // De otra manera, seguiremos usando el default layout.
+        if (product.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("list_visible", true);
+            outState.putString("list_text",
+                    product.getText().toString());
+        }
+    }
+}
+//Cierre de la clase
--- app/src/main/java/com/example/usuario/listadearticulos/listActivity.java
+++ app/src/main/java/com/example/usuario/listadearticulos/listActivity.java
@@ -2,8 +2,6 @@ package com.example.usuario.listadearticulos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
@@ -11,12 +9,17 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 
+/**
+
+ * Esta clase define objetos que maneja la aplicacion, los articulos que trae la misma.
+
+ */
 public class listActivity extends AppCompatActivity {
 
+    //Campos de la clase
     ListView listview;
     ArrayList<String> selectedItems;
     LinkedList<String> ProductList = new LinkedList<>();
@@ -28,10 +31,10 @@ public class listActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
         selectedItems=new ArrayList<String>();
     }
+    //Cierre del método
 
     public void onStart(){
         super.onStart();
-        //  final View vista = mInflater.inflate(R.layout.productlist_item, null);
         for (int i = 0; i < 20; i++) {
             int j = i+1;
             ProductList.addLast("Artículo " + j);
@@ -43,6 +46,9 @@ public class listActivity extends AppCompatActivity {
         listview.setAdapter(adapter);
         listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
 
+            /**
+             * Método que agrega los articulos a seleccionar
+             */
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 // selected item
                 String selectedItem = ((TextView) view).getText().toString();
@@ -52,13 +58,14 @@ public class listActivity extends AppCompatActivity {
                     selectedItems.add(selectedItem); //add selected item to the list of selected items
 
             }
-
-
-
+            //Cierre del método
 
         });
     }
 
+     /**
+     * Método que muestra los articulos seleccionados
+     */
     public void showSelectedItems(View view){
         String selItems="";
         for(String item:selectedItems){
@@ -69,5 +76,7 @@ public class listActivity extends AppCompatActivity {
         }
         Toast.makeText(this, selItems, Toast.LENGTH_LONG).show();
     }
+    //Cierre del método
 
 }
+//Cierre de la clase
